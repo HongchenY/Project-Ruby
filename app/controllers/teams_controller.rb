40dd2c8ee14c317c8256d77fb91d2f80fca34fae
aRorschach <69043531+aRorschach@users.noreply.github.com>
2024-05-02 21:22:49 -0700

    team finished

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,6 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_team, only: [:edit, :update, :destroy]
 
   # GET /teams or /teams.json
   def index
